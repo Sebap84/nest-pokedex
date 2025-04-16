89e2a82c9897849b22346d9231f08c47905d9540
ArielPardo <ariel.pardo.ext@vidacamara.cl>
2025-04-16 13:13:20 -0400

    Fin sección 7

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,15 @@
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+
+export class CreatePokemonDto {
+
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    num: number;
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+}
+
